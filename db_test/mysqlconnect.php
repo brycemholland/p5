5ae67bbec9916215bacd00a88e63d38af64c82a6
Bryce Holland <bholland@performancegpa.com>
2016-03-24 19:41:19 -0500

    first commit

---
+++ db_test/mysqlconnect.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+</head>
+<body>
+
+<?php
+  $conn = mysql_connect("localhost","Bryce","password");
+  if (!$conn){
+  die("Can not Connect: " . mysql_error());
+  }
+?>
+
+</body>
+</html>
